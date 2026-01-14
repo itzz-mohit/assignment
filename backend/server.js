9e04fe7a236235b1b45be725d679a9d6bb1a5184
Mohit <mohitbite@gmail.com>
2026-01-14 23:55:27 +0530

    add cors and mongo atlas

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ import { connectDB } from "./src/config/db.js";
 
 connectDB();
 
+app.get("/", async (req, res) => {
+  res.json({
+    status: "ok",
+    message: "message",
+  });
+});
 
 app.listen(process.env.PORT, () =>
   console.log("Server running on port", process.env.PORT)
--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import chatRoutes from "./routes/chat.routes.js";
 
 const app = express();
-app.use(cors());
+app.use(cors("*"));
 app.use(express.json());
 
 app.use("/api/chat", chatRoutes);
